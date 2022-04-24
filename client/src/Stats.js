1b7fd83107c8d0b180261e12e9428d8d672a1dc5
Manelle Berrichi <manelle.berrichi@etu.sorbonne-universite.fr>
2022-04-24 18:16:28 +0200

    amelioration client

--- client/src/Stats.js
+++ client/src/Stats.js
@@ -1,11 +1,18 @@
+import SearchIcon from '@mui/icons-material/Search';
 import React from 'react'
 import './Stats.css';
+
 class Stats extends React.Component {
     render(){
         return (
             <div className='stats'>
-                <h2>Stats</h2>
-                <input id='searchbar' type='text' name="search" placeholder='Rechercher...'/>
+                <div className='stat_searchbar'>
+                    <SearchIcon className='search_icon'/>
+                    <input type='text' name="search" placeholder='Rechercher...'/>
+                </div>
+                <div className='stat_contenu'>
+                    <h3>What's happening</h3>
+                </div>
             </div>
         );
     }
