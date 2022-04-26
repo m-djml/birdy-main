10fb5dee789087b40c632951008bbcc01dfa14ad
djam1012 <djamila.m.pfsn@gmail.com>
2022-04-26 11:45:43 +0200

    petites modifs

--- server/src/index.js
+++ server/src/index.js
@@ -1,15 +1,16 @@
 const app = require("./app.js");
 const port = 27017;
-app.default.listen(port, () => {
-  console.log(`Serveur actif sur le port ${port}`);
-});
 
 const mongoose = require('mongoose');
 mongoose
 .connect('mongodb://localhost:27017/test', {
-    dbName: 'yourDB-name',
+    /*dbName: 'yourDB-name',*/
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
 .then(() => console.log('MongoDB Connected'))
 .catch(err => console.log('Mongo Error'));
+
+app.default.listen(port, () => {
+  console.log(`Serveur actif sur le port ${port}`);
+});
