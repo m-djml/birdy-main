b939fdfc7be89bd2dbde596994ebac86ea2eea75
djam1012 <djamila.m.pfsn@gmail.com>
2022-05-06 13:41:31 +0200

    suppression des dossiers inutiles

--- my_client/src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-import userReducer from './user_reducer';
-import allUsersReducer from './allusers_reducer';
-
-export default combineReducers({
-    userReducer, allUsersReducer
-})
--- my_serveur_birdy/index.js
+++
@@ -1,7 +0,0 @@
-const app = require("./app");
-
-
-app.listen(process.env.PORT, () => {
-    console.log(`Serveur actif sur le port ${process.env.PORT}`);
-})
-
